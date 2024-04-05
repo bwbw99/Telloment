fca5dd23dada114a9b414f8dd56cc281e33cda91
bwbw99 <bw.seoul.99@gmail.com>
2024-04-05 20:24:49 +0900

    Made UI for grammar check on last tab (2)

--- Telloment/TempViewController.swift
+++ Telloment/TempViewController.swift
@@ -5,4 +5,22 @@
 //  Created by 최병욱 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+class TempViewController:UIViewController{
+    
+    @IBOutlet weak var GrammarTextField: UITextField!
+    
+    
+    @IBAction func GrammarButtonTapped(_ sender: Any) {
+        
+        GrammarTextField.text = "이 문장으로 변경"
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    
+}
