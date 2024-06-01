cdc458175ff5df4b48baf26de78cb680a4133f35
박진영 <tear4321@cau.ac.kr>
2024-06-01 18:16:30 +0900

    play test voice when play button click

---
+++ may24/model/BackendEmotions.swift
@@ -0,0 +1,14 @@
+//
+//  BackendEmotions.swift
+//  may24
+//
+//  Created by 박진영 on 6/1/24.
+//
+
+public enum BackendEmotions: String {
+    
+    case Neutral = "중립"
+    case Happy = "행복"
+    case Sad = "슬픔"
+    case Angry = "화남"
+}
