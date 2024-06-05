47e5e75e2e6d857bc61e5ed213d07e41dd906c4b
박진영 <tear4321@cau.ac.kr>
2024-06-05 16:04:30 +0900

    search API

---
+++ may24/api/dto/response/UserDto.swift
@@ -0,0 +1,15 @@
+//
+//  UserDto.swift
+//  may24
+//
+//  Created by 박진영 on 6/5/24.
+//
+
+import Foundation
+
+struct UserDto: Codable {
+  let name: String
+  let nickName: String
+  let userEmail: String
+  let userId: Int
+}
