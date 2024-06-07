c35453c6d51761aad475c3eaca1a351d743d1e51
박진영 <tear4321@cau.ac.kr>
2024-06-07 10:37:03 +0900

    MIcriphone usage permission

--- may24/api/interceptor/Authentication.swift
+++ may24/api/interceptor/Authentication.swift
@@ -6,6 +6,6 @@
 //
 
 public class Authentication {
-    static var token: String = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5nMTIzIiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcxNzcxOTUxNX0.pY6v9-E6Ac4nQ0AzA8Q5kQ7BnLQRzKGSO_Aczem4alGD3G3gbhXirZebkPlsow6_EJEp-zLw4s82EFrrapTzoA"
-    static var refreshToken: String = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5nMTIzIiwiZXhwIjoxNzE4MzIwNzE1fQ.oeZZMDFkDm_Rt4AO5wFzp62uMAMeqMHzuM4jFYWgzm3upgQYwIpc4xh69bJ8GDvlN6ec2Fb_I79PNO9B-4lp0Q"
+    static var token: String = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MDQiLCJhdXRoIjoiUk9MRV9VU0VSIiwiZXhwIjoxNzE3NzI3NjE2fQ.KKMVuhTMT8I4UNK0QCd04xlMA5xOg1KJdx-twDlQIX9RneaLTeFwFccmIJKUthIZTfhuGX3HQV_w0TyzgAqFMw"
+    static var refreshToken: String = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5nMTIzIiwiZXhwIjoxNzE4MjgyMjU1fQ.WgV9V9HM0euDebH-2253aQjz32vW1kWQ5BnPo-GXi0ynZkpV0elTJ94Yznl8EVLSRjPi-wsgdbPVkqrYQjaQIQ"
 }
